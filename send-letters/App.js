1261577505e5cdd5c558f15c738ab1f905862764
thomasrogers428 <thomas.w.rogers.23@dartmouth.edu>
2023-02-03 13:21:02 -0500

    Separate NavBar into a Component

--- send-letters/App.js
+++ send-letters/App.js
@@ -1,57 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { NavigationContainer } from '@react-navigation/native';
-import Ionicons from '@expo/vector-icons/Ionicons';
 
-import HomeScreen from './screens/HomeScreen';
-import FriendsScreen from './screens/FriendsScreen';
-import ComposeScreen from './screens/ComposeScreen';
-import FontsScreen from './screens/FontsScreen';
-import ProfileScreen from './screens/ProfileScreen';
+import NavBar from './components/NavBar';
 
-
-// Citation: https://reactnavigation.org/docs/tab-based-navigation
-
-const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
       <View style={styles.container}>
         <StatusBar style="auto" />
-        <NavigationContainer>
-          <Tab.Navigator
-            screenOptions={({ route }) => ({
-              headerShown: false, 
-              tabBarShowLabel: false,
-              tabBarIcon: ({ focused, color, size }) => {
-                let iconName;
-
-                if (route.name === 'Home') {
-                  iconName = focused ? 'home': 'home-outline';
-                } else if (route.name === 'Friends') {
-                  iconName = focused ? 'people' : 'people-outline';
-                } else if (route.name === 'Compose') {
-                  iconName = focused ? 'create-sharp' : 'create-outline';
-                } else if (route.name === 'Fonts') {
-                  iconName = focused ? 'pencil' : 'pencil-outline';
-                } else if (route.name === 'Profile') {
-                  iconName = focused ? 'person-circle' : 'person-circle-outline';
-                }
-
-                return <Ionicons name={iconName} size={size} color={color} />;
-              },
-              tabBarActiveTintColor: 'black',
-              tabBarInactiveTintColor: 'gray',   
-            })}
-          >
-            <Tab.Screen name="Home" component={HomeScreen} />
-            <Tab.Screen name="Friends" component={FriendsScreen} />
-            <Tab.Screen name="Compose" component={ComposeScreen} />
-            <Tab.Screen name="Fonts" component={FontsScreen} />
-            <Tab.Screen name="Profile" component={ProfileScreen} />
-          </Tab.Navigator>
-        </NavigationContainer>
+        <NavBar/>
       </View>
   );
 }
