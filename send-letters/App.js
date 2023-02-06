22acb31567c6a844dcd4a92e75d79cfa783432b6
Leah Ryu <ryu.leah@gmail.com>
2023-02-05 19:09:58 -0500

    Add a navigation stack to allow for login screen. Add basic login screen (not connected to backend). Move tab bar to nav stack.

--- send-letters/App.js
+++ send-letters/App.js
@@ -1,15 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
+import LoginScreen from './screens/LoginScreen';
 import NavBar from './components/NavBar';
 
+const Stack = createNativeStackNavigator();
+
+// Header tint color turns icons put inside the header white
+const globalScreenOptions = {
+  headerStyle: { backgroundColor: "#b84a32"},
+  headerTitleStyle: {color: "white"},
+  headerTintColor: "white",
+};
 
 export default function App() {
   return (
-      <View style={styles.container}>
-        <StatusBar style="auto" />
-        <NavBar/>
-      </View>
+    <NavigationContainer>
+      <Stack.Navigator 
+        initialRouteName='Home'
+        screenOptions={globalScreenOptions}>
+        <Stack.Screen 
+          options={{
+            title: "Log in",
+          }}
+          name='Login' 
+          component={LoginScreen}/> 
+        <Stack.Screen name='NavBar' component={NavBar}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
