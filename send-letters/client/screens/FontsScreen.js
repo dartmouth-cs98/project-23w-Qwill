419f668916cd2fd0ba5801604e220f0e2dd887dc
amandajaisun <amanda.j.sun.23@dartmouth.edu>
2023-02-19 19:00:42 -0500

    got fonts to load

--- send-letters/client/screens/FontsScreen.js
+++ send-letters/client/screens/FontsScreen.js
@@ -1,11 +1,29 @@
 import { Text, View } from 'react-native';
+import React, { useState } from "react";
+import AppLoading from 'expo-app-loading';
+import useFonts from '../hooks/useFonts';
 
 function FontsScreen() {
+    const [fontsLoaded, setFontsLoaded] = useState(false);
+    const LoadFonts = async() => {
+        await useFonts();
+    };
+    console.log(fontsLoaded);
+    if (!fontsLoaded) {
+        return(
+            <AppLoading
+                startAsync={LoadFonts}
+                onFinish={() => setFontsLoaded(true)}
+                onError={console.warn}
+            />
+        )
+    };
+
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Fonts</Text>
+        <Text style = {{ fontFamily : 'my_nerve'}}>Fonts</Text>
       </View>
     );
-  }
+  };
 
 export default FontsScreen;
