597baada215a9792b99cf78b51d004b746964a6d
Leah Ryu <ryu.leah@gmail.com>
2023-03-01 21:04:36 -0500

    Add letter context with info like text, recipient, and font/theme IDs so that we can store that information across the compose stack.

--- send-letters/client/screens/compose/SelectFontScreen.js
+++ send-letters/client/screens/compose/SelectFontScreen.js
@@ -1,26 +1,25 @@
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import ButtonPrimary from '../../components/ButtonPrimary';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Ionicons } from '@expo/vector-icons';
 import { composeStackGoBack } from '../../helpers/composeStackGoBack';
+import { ComposeContext } from '../../context/ComposeStackContext';
 
-const SelectFontScreen = ({navigation, route}) => {
-  const {recipientID, recipientUsername, themeID} = route.params;
-  // Default font ID set to 0
-  const [fontID, setFontID] = useState(0);
+const SelectFontScreen = ({navigation}) => {
+
+  const [letterInfo, setLetterInfo] = useContext(ComposeContext);
+
+   // Default font ID set to 0
+   const [fontID, setFontID] = useState(letterInfo.fontID);
 
   const handleNextPressed = () => {
-        navigation.push('ComposeHome', {
-            recipientID: recipientID,
-            recipientUsername: recipientUsername,
-            themeID: themeID, 
-            fontID: fontID
-        });
+      setLetterInfo({...letterInfo, fontID: fontID});
+      navigation.push('ComposeHome');
   };
 
   const selectFontGoBack = () => {
-    navigation.navigate('SelectRecipient');
+    navigation.navigate('SelectTheme');
   }
 
   return (
