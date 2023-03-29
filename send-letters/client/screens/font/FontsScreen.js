f8c64dba23464626c55841a94bfdb5217527db9a
amandajaisun <amanda.j.sun.23@dartmouth.edu>
2023-03-29 16:40:20 -0400

    trying to add camera

--- send-letters/client/screens/font/FontsScreen.js
+++ send-letters/client/screens/font/FontsScreen.js
@@ -1,55 +1,69 @@
-import {Text, View, StyleSheet, FlatList} from 'react-native';
-import React from "react";
+import { Text, View, StyleSheet, FlatList } from 'react-native';
+import React, {useState} from "react";
 import { SafeAreaView } from 'react-native-safe-area-context';
 import FontPreview from '../../components/FontPreview';
 import ButtonCircle from '../../components/ButtonCircle';
+import ButtonPrimary from '../../components/ButtonPrimary';
 import fontData from '../../assets/fontData';
 
+import { Camera, CameraType } from 'expo-camera';
+const [type, setType] = useState(CameraType.back);
+
+function toggleCameraType() {
+  setType(current => (current === CameraType.back ? CameraType.front : CameraType.back));
+}
+
 export default function FontsScreen() {
-    // Keep the splash screen visible while we fetch resources
-    
-    return (
-        <SafeAreaView style={{alignItems: 'center', flex: 1, backgroundColor: "#F0F4FF"}}>
-          <View style={{alignItems: 'center'}}>
-            <View style={{flexDirection: "row", justifyContent: 'space-between', marginTop: 20}}>
-              <Text style={styles.titleText}>Fonts</Text>
-              <ButtonCircle icon="pencil"></ButtonCircle>
-            </View>
-            <View style={{flexDirection: "row", marginTop: 20}}>
-              <View style={styles.line}></View>
-              <Text>Default Fonts</Text>
-              <View style={styles.line}></View>
-            </View>
-            <View style={{flexDirection: "row", marginTop: 20, marginLeft: 20, marginRight: 20}}>
-              <FlatList
-                contentContainerStyle={{flexGrow: 1, justifyContent: 'center'}}
-                data={fontData}
-                numColumns={3}
-                renderItem={({item}) => 
-                  <View style={{marginLeft: 10, marginRight: 10}}>
-                    <FontPreview style={item.style} title={item.title}></FontPreview>
-                  </View>}
-                keyExtractor={(item) => item.title}
-              />
-            </View>
-            <View style={{flexDirection: "row", marginTop: 20}}>
-              <View style={styles.line}></View>
-              <Text>Custom Fonts</Text>
-              <View style={styles.line}></View>
-            </View>
-            <View style={styles.noCustom}>
-              <Text style={{textAlign: 'center', marginTop: 20}}>You don't have any custom fonts yet.</Text>
-              <Text style={{textAlign: "center", textDecorationLine: 'underline', marginTop: 20}}>Add a custom font</Text>
-            </View>
-          </View>
-        </SafeAreaView>
-    );
+  // Keep the splash screen visible while we fetch resources
+
+  return (
+    <SafeAreaView style={{ alignItems: 'center', flex: 1, backgroundColor: "#F0F4FF" }}>
+      <View style={{ alignItems: 'center' }}>
+
+        <View style={{ flexDirection: "row", justifyContent: 'space-between', marginTop: 20 }}>
+          <Text style={styles.titleText}>Fonts</Text>
+          <ButtonCircle icon="pencil"></ButtonCircle>
+        </View>
+        <View style={{ flexDirection: "row", marginTop: 20 }}>
+          <View style={styles.line}></View>
+          <Text>Default Fonts</Text>
+          <View style={styles.line}></View>
+        </View>
+        <View style={{ flexDirection: "row", marginTop: 20, marginLeft: 20, marginRight: 20 }}>
+          <FlatList
+            contentContainerStyle={{ flexGrow: 1, justifyContent: 'center' }}
+            data={fontData}
+            numColumns={3}
+            renderItem={({ item }) =>
+              <View style={{ marginLeft: 10, marginRight: 10 }}>
+                <FontPreview style={item.style} title={item.title}></FontPreview>
+              </View>}
+            keyExtractor={(item) => item.title}
+          />
+        </View>
+        <View style={{ flexDirection: "row", marginTop: 20 }}>
+          <View style={styles.line}></View>
+          <Text>Custom Fonts</Text>
+          <View style={styles.line}></View>
+        </View>
+        <View style={styles.noCustom}>
+        <Text style={{ textAlign: 'center', marginTop: 20 }}>You don't have any custom fonts yet.</Text>
+          {/* <Text style={{ textAlign: "center", textDecorationLine: 'underline', marginTop: 20 }}>Add a custom font</Text> */}
+          <ButtonPrimary
+            selected={false}
+            title={"Add custom font"}
+            onPress={toggleCameraType}
+          />
+        </View>
+      </View>
+    </SafeAreaView>
+  );
 };
 
 const styles = StyleSheet.create({
   titleText: {
     fontFamily: 'JosefinSansBold',
-    fontSize: 50, 
+    fontSize: 50,
     fontWeight: 'bold',
     textAlign: 'left',
     flex: 1,
