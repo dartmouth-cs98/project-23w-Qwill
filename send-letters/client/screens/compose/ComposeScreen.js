acb9666362d6a0f45f27aa3597309d49606e8bcc
Pierce Wilson <53098685+piercedwilson@users.noreply.github.com>
2023-04-03 16:22:15 -0400

    tap to dismiss keyboard on compose screen

--- send-letters/client/screens/compose/ComposeScreen.js
+++ send-letters/client/screens/compose/ComposeScreen.js
@@ -1,4 +1,4 @@
-import { Text, View, StyleSheet, Dimensions, ImageBackground, ScrollView} from 'react-native';
+import { Text, View, StyleSheet, Dimensions, ImageBackground, ScrollView, TouchableWithoutFeedback, Keyboard,} from 'react-native';
 import React, { useState, useLayoutEffect, useEffect, useContext } from 'react'
 import { Button, Input, Image } from 'react-native-elements';
 import ButtonPrimary from '../../components/ButtonPrimary';
@@ -28,15 +28,24 @@ function ComposeScreen({ navigation, route }) {
         resizeMode={'cover'}
         style={{ flex: 1, width: '100%', height: '95%'}} 
         source={images.themes[letterInfo.themeID]}> 
-          <Input
-            style={{fontFamily: letterInfo.fontID, marginTop: 20, fontSize: 22}} 
-            placeholder={"Start writing your letter!"}
-            defaultValue={text}
-            inputContainerStyle={{borderBottomWidth:0}}
-            onChangeText={text => setLetterInfo({...letterInfo, text: text})}
-            multiline={true}
-            autoCapitalize='none'
-          />
+          {
+          <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+            <View style={{flex: 1}}>
+                <Input
+                style={{fontFamily: letterInfo.fontID, marginTop: 20, fontSize: 22}} 
+                placeholder={"Start writing your letter!"}
+                defaultValue={text}
+                inputContainerStyle={{borderBottomWidth:0}}
+                onChangeText={text => setLetterInfo({...letterInfo, text: text})}
+                multiline={true}
+                autoCapitalize='none'
+                keyboard='default'
+              /> 
+            </View>
+          </TouchableWithoutFeedback>
+          
+          
+}
       </ImageBackground>
       <View style={{flexDirection: 'row'}}>
         <ButtonPrimary title={"Go back"} selected={true} onPress={() => composeStackGoBack(navigation, composeHomeGoBack)}/>
