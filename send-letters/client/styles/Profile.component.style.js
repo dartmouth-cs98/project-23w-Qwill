df31da341be1afbbb2c89101b719444548dd660a
amandajaisun <amanda.j.sun.23@dartmouth.edu>
2023-04-25 18:19:36 -0400

    alphabetized styles

--- send-letters/client/styles/Profile.component.style.js
+++ send-letters/client/styles/Profile.component.style.js
@@ -3,18 +3,61 @@ import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-nat
 // Separating styles from component code src: https://www.reactnative.guide/8-styling/8.3-separating-styles-from-component.html
 
 export default StyleSheet.create({
+  button: {
+    width: 200,
+    marginTop: 10,
+  },
+  container: {
+    height: hp('10.08%'), // (94/932) * 100% ≈ 10.08% of screen height
+    width: wp('72.56%'), // (312/430) * 100% ≈ 72.56% of screen width
+    backgroundColor: '#97ACE2',
+    borderRadius: 20,
+    marginBottom: hp('1.61%'), // (15/932) * 100% ≈ 1.61% of screen height
+  },
   fontsContainer: {
     width: wp('82%'),
     height: hp('80%'),
     borderRadius: 20,
     flex: 1,
   },
-  titleText: {
-    fontSize: wp('10%'),
-    fontFamily: 'JosefinSansBold',
-    fontWeight: 'bold',
+  letterContainer: {
+    position: 'absolute',
+    left: wp('23%'), // (100/430) * 100% ≈ 23.26% of screen width
+    top: 0,
+  },
+  inputContainer: {
+    width: 285,
+    marginLeft: 30,
+  },
+  line: {
+    width: wp('25.58%'), // (110/430) * 100% ≈ 25.58% of screen width
+    height: 0,
+    borderWidth: 1,
+    borderColor: '#737B7D',
+    marginLeft: wp('2.33%'), // (10/430) * 100% ≈ 2.33% of screen width
+    marginRight: wp('2.33%'), // (10/430) * 100% ≈ 2.33% of screen width
+    marginTop: hp('0.86%'), // (8/932) * 100% ≈ 0.86% of screen height
+  },
+  profilePicture: {
+    height: hp('6.01%'), // (56/932) * 100% ≈ 6.01% of screen height
+    width: wp('13.02%'), // (56/430) * 100% ≈ 13.02% of screen width
+    borderRadius: wp('6.51%'), // (28/430) * 100% ≈ 6.51% of screen width
+    backgroundColor: '#000000',
+    position: 'absolute',
+    left: wp('4.19%'), // (18/430) * 100% ≈ 4.19% of screen width
+    top: hp('1.18%'), // (11/932) * 100% ≈ 1.18% of screen height
+  },
+  recipientsContainer: {
+    width: 350,
+    height: 585,
+    borderRadius: 20,
     flex: 1,
-    textAlign: 'center',
+  },
+  safeview: {
+    flexDirection: 'column', flex: 1, alignItems: 'center'
+  },
+  scrollView: {
+    height: 200,
   },
   selectTitleText: {
     marginBottom: hp('4%'),
@@ -29,30 +72,12 @@ export default StyleSheet.create({
     shadowOpacity: 0.2,
     shadowRadius: 3,
   },
-  line: {
-    width: wp('25.58%'), // (110/430) * 100% ≈ 25.58% of screen width
-    height: 0,
-    borderWidth: 1,
-    borderColor: '#737B7D',
-    marginLeft: wp('2.33%'), // (10/430) * 100% ≈ 2.33% of screen width
-    marginRight: wp('2.33%'), // (10/430) * 100% ≈ 2.33% of screen width
-    marginTop: hp('0.86%'), // (8/932) * 100% ≈ 0.86% of screen height
-  },
-  container: {
-    height: hp('10.08%'), // (94/932) * 100% ≈ 10.08% of screen height
-    width: wp('72.56%'), // (312/430) * 100% ≈ 72.56% of screen width
-    backgroundColor: '#97ACE2',
-    borderRadius: 20,
-    marginBottom: hp('1.61%'), // (15/932) * 100% ≈ 1.61% of screen height
-  },
-  profilePicture: {
-    height: hp('6.01%'), // (56/932) * 100% ≈ 6.01% of screen height
-    width: wp('13.02%'), // (56/430) * 100% ≈ 13.02% of screen width
-    borderRadius: wp('6.51%'), // (28/430) * 100% ≈ 6.51% of screen width
-    backgroundColor: '#000000',
-    position: 'absolute',
-    left: wp('4.19%'), // (18/430) * 100% ≈ 4.19% of screen width
-    top: hp('1.18%'), // (11/932) * 100% ≈ 1.18% of screen height
+  titleText: {
+    fontSize: wp('10%'),
+    fontFamily: 'JosefinSansBold',
+    fontWeight: 'bold',
+    flex: 1,
+    textAlign: 'center',
   },
   username: {
     fontSize: wp('2.5%'), // (11/430) * 100% ≈ 2.56% of screen width
@@ -60,29 +85,4 @@ export default StyleSheet.create({
     top: hp('8%'), // (72/932) * 100% ≈ 7.73% of screen height
     left: wp('4%'), // (18/430) * 100% ≈ 4.19% of screen width
   },
-  letterContainer: {
-    position: 'absolute',
-    left: wp('23%'), // (100/430) * 100% ≈ 23.26% of screen width
-    top: 0,
-  },
-  button: {
-    width: 200,
-    marginTop: 10,
-  },
-  scrollView: {
-    height: 200,
-  },
-  recipientsContainer: {
-    width: 350,
-    height: 585,
-    borderRadius: 20,
-    flex: 1,
-  },
-  inputContainer: {
-    width: 285,
-    marginLeft: 30,
-  },
-  safeview: {
-    flexDirection: 'column', flex: 1, alignItems: 'center'
-  }
 });
