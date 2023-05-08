84b25da825d13309d201ef6d2303fe1e8c20cd1d
amandajaisun <amanda.j.sun.23@dartmouth.edu>
2023-05-08 14:44:03 -0400

    converted blue button

--- send-letters/client/components/ButtonBlue.components.js
+++ send-letters/client/components/ButtonBlue.components.js
@@ -13,57 +13,29 @@ const ButtonBlue = props => {
       <Text style={[styles.unselectedText]}>{props.title}</Text>
     </TouchableOpacity>
   );
-  // const selected = props.selected;
 
-  // // If the button's text is longer, we can make the button a bit wider
-  // const textWidth = props.textWidth ? props.textWidth : 300;
-
-  // // add in a margin top if wanted
-  // const marginTop = props.marginTop ? props.marginTop : 0;
-
-  // if (selected) {
-  //   return (
-  //     <TouchableOpacity style={[styles.selectedBtn]} onPress={props.onPress}>
-  //       <Text style={[styles.selectedText]}>{props.title}</Text>
-  //     </TouchableOpacity>
-  //   );
-  // }
-  // else {
-  //   return (
-  //     <TouchableOpacity
-  //       style={[styles.btn, styles.unselectedBtn, { marginTop: marginTop }]}
-  //       onPress={props.onPress}
-  //     >
-  //       <Text style={[styles.unselectedText]}>{props.title}</Text>
-  //     </TouchableOpacity>
-  //   );
-  // }
 };
-
 const styles = StyleSheet.create({
   selectedText: {
     fontFamily: "Mulish-Italic",
-    fontWeight: "heavy",
-    fontSize: 15,
-    letterSpacing: 0.3,
+    fontWeight: "bold",
+    fontSize: wp('3.2%'),
+    letterSpacing: wp('0.3%'),
     color: COLORS.blue400,
   },
   unselectedText: {
     fontFamily: "Mulish",
-    fontWeight: "900",
-    fontSize: 15,
-    letterSpacing: 0.3,
+    fontWeight: "bold",
+    fontSize: wp('3.2%'),
+    letterSpacing: wp('0.3%'),
     color: COLORS.blue800,
   },
   btn: {
     alignItems: "center",
-    padding: 16,
-    borderTopLeftRadius: 25,
-    borderTopRightRadius: 25,
-    borderBottomRightRadius: 25,
-    borderBottomLeftRadius: 25,
-    marginRight: 10,
-    marginLeft: 10,
+    paddingVertical: hp('1.5%'),
+    paddingHorizontal: wp('8%'),
+    borderRadius: wp('5%'),
+    marginHorizontal: wp('2%'),
   },
   selectedBtn: {
     backgroundColor: COLORS.blue700,
@@ -72,5 +44,4 @@ const styles = StyleSheet.create({
     backgroundColor: COLORS.blue400,
   }
 });
-
 export default ButtonBlue;
